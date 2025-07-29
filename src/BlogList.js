8ce77ae11f5ee57150ef1df5cbf854f716ae63c7
Nik Farees <nikfarees2015@gmail.com>
2025-07-29 12:04:51 +0800

    #12 - Functions as Props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,4 @@
-// const BlogList = (props) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
-
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
 
     return (
         <div className="blog-list">
@@ -11,6 +7,7 @@ const BlogList = ({ blogs, title }) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
         </div>
