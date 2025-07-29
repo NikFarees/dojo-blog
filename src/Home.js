1bf17587c5418dd072868ffb3534781bf8da2254
Nik Farees <nikfarees2015@gmail.com>
2025-07-29 11:39:11 +0800

    #11 - Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blogs) => blogs.author === 'Mario')} title="Mario's blogs"/>
         </div>
     );
 }
