e21b30d2ac8849b816e7240e0c80dc25d56f6789
Nik Farees <nikfarees2015@gmail.com>
2025-07-29 12:31:14 +0800

    #15 - Using JSON Server

--- src/Home.js
+++ src/Home.js
@@ -8,26 +8,18 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', author: 'Mario', id: 3 },
     ]);
 
-    const [name, setName] = useState('mario');
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
-    // This effect runs every time the component renders
-    // If you want it to run only once, pass an empty array as the second argument
-    // If you want it to run when specific variables change, include them in the array
     useEffect(() => {
         console.log('use effect ran');
-        console.log(name);
-    }, [name]);
+    }, []);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            <button onClick={() => setName('luigi')}>Change name</button>
-            <p>{ name }</p>
         </div>
     );
 }
